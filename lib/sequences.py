54d92569ecdfcd359d6807e634a35851851642b1
Jesse Ilc <jesseilc123@gmail.com>
2023-05-21 13:16:18 -0500

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    FibArray  = [0, 1]
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        count = 1
+        for i in range(length - 2):
+            FibArray.append(count + FibArray[i])
+            count = count + FibArray[i]
+        print(FibArray)
